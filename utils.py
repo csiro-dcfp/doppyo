c107fc2e72f6cc4cbeab964265c6bb6ebfb345a0
Squire <squ027@oa-31-cdc.nexus.csiro.au>
2018-04-12 18:04:44 +1000

    added clim/persis functionality to pylatte

--- utils.py
+++ utils.py
@@ -6,13 +6,17 @@
 """
 
 __all__ = ['categorize','compute_pdf', 'compute_cdf', 'compute_rank', 'compute_histogram',
-           'calc_integral', 'calc_difference', 'calc_division', 'calc_boxavg_latlon',
-           'get_nearest_point', 'make_lon_positive', 'get_bin_edges', 'timer', 'find_other_dims']
+           'calc_integral', 'calc_difference', 'calc_division', 'load_climatology', 'anomalize',
+           'leadtime_to_datetime', 'datetime_to_leadtime', 'repeat_data', 'calc_boxavg_latlon',
+           'stack_by_init_date', 'prune', 'get_nearest_point', 'make_lon_positive', 'get_bin_edges', 
+           'get_lead_times', 'timer', 'find_other_dims']
 
 # ===================================================================================================
 # Packages
 # ===================================================================================================
+from datetime import timedelta
 import numpy as np
+import pandas as pd
 import xarray as xr
 import time
 
@@ -148,7 +152,109 @@ def calc_division(fcst,obsv):
 
 
 # ===================================================================================================
+# Climatology tools
+# ===================================================================================================
+def load_climatology(clim, variable, freq):
+    """ 
+    Returns pre-saved climatology at desired frequency 
+    Current frequency options are: daily ('D'); monthly ('M'); yearly ('Y')
+    """
+    
+    # Load specified dataset -----
+    if clim == 'jra_1958-2016':
+        data_loc = '/OSM/CBR/OA_DCFP/data/intermediate_products/pylatte_climatologies/jra.55.isobaric.1958010100_2016123118.clim.nc'
+
+        ds = xr.open_mfdataset(data_loc, autoclose=True)
+        
+        if variable not in ds.data_vars:
+            raise ValueError(f'"{variable}" is not (yet) available in {clim}')
+            
+    else:
+        raise ValueError(f'"{clim}" is not an available climatology. Available options are "jra_1958-2016"')
+    
+        
+    if variable == 'precip':
+        clim = ds[variable].resample(freq=freq, dim='time', how='sum')
+    else:
+        clim = ds[variable].resample(freq=freq, dim='time', how='mean')
+        
+    clim['time'] = clim['time'].values.astype('<M8[' + freq + ']')
+
+    return clim
+
+
+# ===================================================================================================
+def anomalize(data,clim):
+    """ Receives raw and climatology data at matched frequencies and returns the anomaly """
+    
+    data_freq = pd.infer_freq(data.time.values)
+    clim_freq = pd.infer_freq(clim.time.values)
+    
+    if data_freq != clim_freq:
+        raise ValueError('"data" and "clim" must have matched frequencies')
+    
+    if (data_freq == 'M') | (data_freq == 'MS'):
+        freq = 'time.month'
+    elif data_freq == 'D':
+        freq = 'time.day'
+        
+    anom = data.groupby(freq) - clim.groupby(freq).mean()
+    
+    return prune(anom)
+
+# ===================================================================================================
 # IO tools
+# ===================================================================================================
+def leadtime_to_datetime(data):
+    """ Converts time information from init_date/lead_time dimension pair to single datetime dimension """
+    
+    init_date = data.init_date.values
+    lead_time= list(map(int,data.lead_time.values))
+    freq = data.lead_time.attrs['units']
+
+    if freq == 'MS':
+        freq = 'M'
+
+    datetime = np.array([init_date.astype('<M8[' + freq + ']') + 
+                         np.timedelta64(ix, freq) for ix in lead_time]) \
+                         .astype('<M8[' + freq + ']')
+    
+    data = data.rename({'lead_time' : 'time'})
+    data['time'] = datetime
+    
+    return prune(data)
+
+
+# ===================================================================================================
+def datetime_to_leadtime(data):
+    """ Converts time information from single datetime dimension to init_date/lead_time dimension pair """
+    
+    init_date = data.time.values[0]
+    lead_times = range(len(data.time))
+    freq = pd.infer_freq(data.time.values)
+
+    data = data.rename({'time' : 'lead_time'})
+    data['lead_time'] = lead_times
+    data['lead_time'].attrs['units'] = freq
+
+    data.coords['init_date'] = init_date
+    
+    return prune(data)
+
+
+# ===================================================================================================
+def repeat_data(data, repeat_dim, repeat_dim_value=0):
+    """ 
+    Returns object the same sizes as data, but with data at repeat_dim = repeat_dim_value repeated across all 
+    other entries in repeat_dim
+    """
+
+    repeat_data = data.loc[{repeat_dim : repeat_dim_value}].drop(repeat_dim).squeeze()
+    
+    return (0 * data).groupby(repeat_dim,squeeze=True).apply(calc_difference, obsv=-repeat_data)
+
+
+
 # ===================================================================================================
 def calc_boxavg_latlon(da,box):
     '''
@@ -169,6 +275,53 @@ def calc_boxavg_latlon(da,box):
     da = da.mean(dim=('lat','lon'))
     
     return da
+
+
+# ===================================================================================================
+def stack_by_init_date(data_in, init_dates, N_lead_steps, init_date_name='init_date', lead_time_name='lead_time'):
+    """ 
+    Splits provided data array into n chunks beginning at time=init_date[n] and spanning 
+    N_lead_steps time increments
+    Input Dataset/DataArray must span full range of times required for this operation
+    """
+
+    # Initialize xarray object for first initialization date -----
+    start_index = np.where(data_in.time == np.datetime64(init_dates[0]))[0].item()
+    data_out = data_in.isel(time=range(start_index, start_index + N_lead_steps))
+    data_out.coords[init_date_name] = init_dates[0]
+    data_out = data_out.expand_dims(init_date_name)
+    data_out = data_out.rename({'time' : lead_time_name})
+    data_out[lead_time_name] = range(N_lead_steps)
+    data_out[lead_time_name].attrs['units'] = pd.infer_freq(data_in.time.values)
+    
+    # Loop over remaining initialization dates -----
+    for init_date in init_dates[1:]:
+        start_index = np.where(data_in.time == np.datetime64(init_date))[0].item()
+        data_temp = data_in.isel(time=range(start_index, start_index + N_lead_steps))
+
+        # Concatenate along initialization date dimension/coordinate -----
+        data_temp = data_temp.rename({'time' : lead_time_name})
+        data_temp[lead_time_name] = range(N_lead_steps)
+        data_temp.coords[init_date_name] = init_date
+        data_out = xr.concat([data_out, data_temp],init_date_name) 
+    
+    return data_out
+
+
+# ===================================================================================================
+def prune(data,squeeze=False):
+    """ Removes all coordinates that are not dimensions from an xarray object """
+    
+    codims = list(set(data.coords)-set(data.dims))
+
+    for codim in codims:
+        if codim in data.coords:
+            data = data.drop(codim)
+        
+    if squeeze:
+        data = data.squeeze()
+    
+    return data
 
 
 # ===================================================================================================
@@ -198,6 +351,21 @@ def get_bin_edges(bins):
                                  [bins[-1]+dbin[-1]]))
     
     return bin_edges
+
+
+# ===================================================================================================
+def get_lead_times(FCST_LENGTH, resample_freq):
+    """ 
+    Returns range() of the minimum number of time increments at resample_freq 
+    over the period of years, FCST_LENGTH
+    """
+
+    no_leap = 2001
+    n_incr = len(pd.date_range('1/1/' + str(no_leap),
+                               '12/1/' + str(no_leap+FCST_LENGTH-1),
+                               freq=resample_freq)) # number of lead_time increments
+    
+    return range(n_incr+1)
 
 
 # ===================================================================================================
